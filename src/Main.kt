f3ae6e3a086eaa101e0e5301f87b3c7cf422ff96
Shirleen19 <muthonishirleen2002@gmail.com>
2022-03-02 11:45:35 +0300

    Functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,147 @@
-fun main() {
-    helloName()
-    myself()
-    add(num1=67,num2=33)
-    add(num1=45,num2=989)
-    add(num1=7000,num2=4560)
-    add(num1=807,num2=420)
-    remainder(a=989,b=47)
+//fun main() {
+////   var message=helloName(" Shirleen")
+////    var m=myself("I am good with remembering names")
+////    var result1=add(num1=63,num2=98)
+////   var result2 =add(num1=45,num2=989)
+////    var result3=add(num1=7000,num2=4560)
+////   var result4= add(num1=807,num2=420)
+////   var answer=modulus(a=989,b=47)
+//    var name=fullName("Elizabeth","Kurira")
+////
+////    println(message)
+////    println(m)
+////    println(result1)
+////    println(result2)
+////    println(result3)
+////    println(result4)
+////    println(answer
+//    println(name)
+//}
+
+
+
+//fun helloName(name:String):String{
+//    return ("Hello"+name)
+//}
+
+//fun myself(message:String):String{
+//var m=(message)
+//    return m
+//fun main() {
+////   var message=helloName(" Shirleen")
+////    var m=myself("I am good with remembering names")
+////    var result1=add(num1=63,num2=98)
+////   var result2 =add(num1=45,num2=989)
+////    var result3=add(num1=7000,num2=4560)
+////   var result4= add(num1=807,num2=420)
+////   var answer=modulus(a=989,b=47)
+//    var name=fullName("Elizabeth","Kurira")
+////
+////    println(message)
+////    println(m)
+////    println(result1)
+////    println(result2)
+////    println(result3)
+////    println(result4)
+////    println(answer
+//    println(name)
+//}
+
+
+
+//fun helloName(name:String):String{
+//    return ("Hello"+name)
+//}
+
+//fun myself(message:String):String{
+//var m=(message)
+//    return m
+
+//}
+//fun add(num1:Int,num2:Int):Int{
+//    var sum=num1+num2
+//    return sum
+
+//}
+
+//fun modulus(a:Int,b:Int):Int {
+//    val remainder = a % b
+//    return remainder
+
+//}
+
+
+//fun fullName(firstName:String,surName:String):String{
+
+//    var name="$firstName $surName"
+//    return name
+//}
+
+
+
+//fun main(){
+//    var statement=fruitNumber("bananas",6)
+//    print(statement)
+//}
+//
+//fun fruitNumber(message:String,num:Int):String{
+//    var mix=("I have $num $message")
+//    return mix
+//}
+
+
+fun main(){
+    var sum=add(6,28,42)
+println(sum)
+    var result=multiply(74,36,20)
+    println(result)
+    Char()
+    var text=myAge("Shirleen",20)
+    println(text)
+     self()
+    myName()
 }
 
-
-
-fun helloName(){
-    println("Hello Shirleen Gathungu")
+fun add(num1:Int,num2:Int,num3:Int):Int{
+    var sum = num1+num2+num3
+    return sum
 }
 
-fun myself(){
-    val message:String=("My name is Shirleen Gathungu.Currently on a gap year.I just realised in the past year that I am very good with remembering details in a person like their full name.I am also very good in imagining unrealistic scenarios.I believe with this kind of imagination,I will be able to innovate amazing things in tech that will be useful to humans.")
-    println(message)
-}
-fun add(num1:Int,num2:Int){
-    num1+num2
-    println(num1+num2)
+fun multiply(num1:Int,num2:Int,num3:Int):Int{
+    var product=num1*num2*num3
+    return product
 }
 
-fun remainder(a:Int,b:Int) {
-    val remainder=a % b
-    println(remainder)
+fun self(){
+    var age=20
+
+    var statement="I am $age years old"
+    println(statement)
+}
+fun Char(){
+    var name="Doris"
+    println(name[1])
+    println(name[3])
+    println(name[4])
+
+}
+fun myAge(word:String,age:Int):String{
+    var word="Shirleen"
+    var age=20
+    var statement="Hi,my name is $word,I am $age year old"
+    return statement
+    }
+
+fun stringLength() {
+    var name = "Shirleen"
+  println(name.length)
+
+}
+
+fun myName(){
+ var name="Shirleen"
+
+
 
 
 }
